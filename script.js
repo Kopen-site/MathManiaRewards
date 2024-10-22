0bbd0b2f58f208bc7250b007b134ed2304d391c6
Kopen <Kopen.gg.site@gmail.com>
2024-10-22 15:03:17 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MathMania Rewards</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Nagrody</h1>
+        <ul>
+            <li>5 Minut – 5 Punktów</li>
+            <li>30 Minut – 30 Punktów</li>
+            <li>40 Minut – 40 Punktów</li>
+            <li>60 Minut – 60 Punktów</li>
+        </ul>
+        <button onclick="window.location.href='https://kopen-site.github.io/MathMania/'">Powróć na stronę główną</button>
+    </div>
+    <script src="script.js"></script>
+</body>
+</html>
